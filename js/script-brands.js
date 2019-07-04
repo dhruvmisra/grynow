badef9fbb9ff005f4b99c6109c53ba7a2dbf5e93
Dhruv <dhruvmisra@live.com>
2019-07-04 15:54:11 +0530

    Started adding content

---
+++ js/script-brands.js
@@ -0,0 +1,51 @@
+let stepsImages = document.getElementsByClassName('steps-img')[0].getElementsByClassName('sec-img');
+let steps = document.getElementsByClassName('accordion')[0].getElementsByClassName('card');
+
+console.log(steps);
+
+Array.prototype.forEach.call(steps, e => {
+    e.addEventListener('click', showToggle);
+})
+
+
+function showToggle() {
+    let step = event.currentTarget;
+    let content = step.getElementsByClassName('collapse')[0];
+    let currentImage;
+
+    for(let i=0; i<stepsImages.length; i++) {
+        if(stepsImages[i].classList.contains('show'))
+            currentImage = stepsImages[i];
+    }
+
+    console.log(currentImage);
+
+    setTimeout(() => {
+        console.log(content.id);
+        if(content.classList.contains('show')) {
+            switch(content.id) {
+                case 'collapseOne' : 
+                    console.log('true');
+                    currentImage.classList.remove('show');
+                    stepsImages[0].classList.add('show');
+                    break;
+                case 'collapseTwo' : 
+                    currentImage.classList.remove('show');
+                    stepsImages[1].classList.add('show');
+                    break;
+                case 'collapseThree' : 
+                    currentImage.classList.remove('show');
+                    stepsImages[2].classList.add('show');
+                    break;
+                case 'collapseFour' : 
+                    currentImage.classList.remove('show');
+                    stepsImages[3].classList.add('show');
+                    break;
+                case 'collapseFive' : 
+                    currentImage.classList.remove('show');
+                    stepsImages[4].classList.add('show');
+                    break;
+            }
+        }
+    }, 500)
+}
--- js/script.js
+++ js/script.js
@@ -168,3 +168,21 @@ $(document).ready(function(){
       }]
   });
 });
+$(document).ready(function(){
+  $('.slick-clients').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: false
+        }
+      }]
+  });
+});
