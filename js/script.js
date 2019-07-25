cd6b7360e397a1827ef7b2fdd337524f9a863e9b
Dhruv <dhruvmisra@live.com>
2019-07-25 16:52:25 +0530

    Added 2 category pages

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 //Read more
 let read = document.getElementById('read-more');
+let content = document.getElementById('read-content');
+content.style.display = 'none';
 read.addEventListener('click', () => {
-  let content = document.getElementById('read-content');
+  console.log('clicked');
   if(content.style.display == 'none') {
+    console.log('if');
     content.style.display = 'block';
     read.innerHTML = 'Read Less';
   } else {
+    console.log('else');
     content.style.display = 'none';
     read.innerHTML = 'Read More';
   }
-})
+});
 
 //Features
 let features = document.getElementsByClassName('feature');
